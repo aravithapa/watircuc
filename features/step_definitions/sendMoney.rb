1abe81780e9f07fdb852609d9d52f1ca56db0c75
Ravi Thapa <aravithapa@gmail.com>
2018-01-16 16:58:26 +0545

    google search scenerio

--- features/step_definitions/sendMoney.rb
+++ features/step_definitions/sendMoney.rb
@@ -3,18 +3,17 @@ require 'watir'
 
 Given(/^that I have gone to the Google page$/) do
   @browser = Watir::Browser.new :chrome
-  @browser.goto "http://www.google.com"
   @browser.window.maximize
-  sleep 5
+  @browser.goto "https://www.amazon.com"
 end
 
-When(/^I add "([^"])" to the search box$/) do |arg|
-  @browser.find_element(:name,'q').send_keys(arg)
+When(/^I add "([^"]*)" to the search box$/) do |arg|
+  @browser.text_field(:id => "twotabsearchtextbox").send_keys(arg)
   sleep 3
 end
 
 And(/^click the Search Button$/) do
-  @browser.find_element(:name, 'btnG').click
+  @browser.find_element(:vlaue, 'Go').click
 end
 
 Then(/^"([^"]*)" should be mentioned in the results$/) do |item|
